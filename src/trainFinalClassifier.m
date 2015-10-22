bf3e846749ef459020dc8d9b1c46ddc74893206f
palexang <palexang@auth.gr>
2015-10-22 19:16:54 +0300

    Train final classifier

--- src/trainFinalClassifier.m
+++ src/trainFinalClassifier.m
@@ -1,31 +1,54 @@
-%  clear; clc;
- 
+function [Xtest, Ytest] = trainFinalClassifier()
 % Read class labels from file
-load('matlab.mat');
-fid = fopen('data/meta/train.txt');
-trImages = textscan(fid, '%s', 'Delimiter', '\n');
-trImages = trImages{1};
-% numTrImages = length(trImages);
-numTrImages = 10;
-
+load('matlab.mat', 'classes');
 load('models.mat');
-load('params.mat');
-
+params = load('encoding_params.mat');
 pyramidLevels = 3;
 params.pyramidLevels = pyramidLevels;
+params.classes = classes;
+
+% [Xtrain, Ytrain] = createTraining(models, params);
+[Xtest, Ytest] = createTest(models, params);
+% model = train(double(Ytrain), sparse(double(Xtrain)), '-s 3 -q');
+% modelEval = evaluateModel(model, Xtest, Ytest);
+% fprintf('Accuracy = %f\n', modelEval(1));
+end
+
+function [X,y] = createTraining(models, params)
+fid = fopen('data/meta/train.txt');
+trainImages = textscan(fid, '%s', 'Delimiter', '\n');
+trainImages = trainImages{1};
+
+[X, y] = encodeImageSet(trainImages, models, params);
+end
+
+function [X,y] = createTest(models, params)
+fid = fopen('data/meta/test.txt');
+testImages = textscan(fid, '%s', 'Delimiter', '\n');
+testImages = testImages{1};
+
+[X, y] = encodeImageSet(testImages, models, params);
+end
+
+
+function [X,y] = encodeImageSet(imgSet, models, params)
+
+pyramidLevels = params.pyramidLevels;
+classes = params.classes;
+numImages = length(imgSet);
 
 [numClasses, numComponents] = size(models);
 numCells = sum(4 .^ (0:pyramidLevels-1)); % Num of cells in pyramid grid
 d = numClasses * numComponents * numCells; % Dimensionality of feature vec
 
-X = single(zeros(numTrImages, d));
-y = single(zeros(numTrImages, 1));
+X = single(zeros(numImages, d));
+y = uint8(zeros(numImages, 1));
 
-for i = 1:numTrImages
+for i = 1:numImages
     try
         tic
-        fprintf('%d/%d ', i, numTrImages);
-        str = num2str(cell2mat(trImages(i)));
+        fprintf('%d/%d ', i, numImages);
+        str = num2str(cell2mat(imgSet(i)));
         split = strsplit(str, '/');
         class = num2str(cell2mat(split(1)));
         imgPath = ['data/images/' str '.jpg'];
@@ -37,4 +60,5 @@ for i = 1:numTrImages
         disp(getReport(ME,'extended'));
     end
 
+end
 end
